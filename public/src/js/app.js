a61cbf6ef0f6b4a6a5d23e26f0a9f47ce922ac03
clarmso <clare.m.so+github@gmail.com>
2021-03-02 23:49:48 -0500

    Add values for my own deployment

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,14 +1,14 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCbhU8BmjoxnZ128tZyRUKc4U2p4Q0j9Iw",
-    authDomain: "ecommerce-with-firebase-a7af6.firebaseapp.com",
-    databaseURL: "https://ecommerce-with-firebase-a7af6.firebaseio.com",
-    projectId: "ecommerce-with-firebase-a7af6",
-    storageBucket: "ecommerce-with-firebase-a7af6.appspot.com",
-    messagingSenderId: "1086804382186",
-    appId: "1:1086804382186:web:c8ca6c306569919e132293",
-    measurementId: "G-7FEL1DERZ2"
+  apiKey: "AIzaSyCEqtcTnqnW3JWbfKqe58iuWiNvYUTjFTc",
+  authDomain: "hello-firebase-87ca6.firebaseapp.com",
+  databaseURL: "https://hello-firebase-87ca6.firebaseio.com",
+  projectId: "hello-firebase-87ca6",
+  storageBucket: "hello-firebase-87ca6.appspot.com",
+  messagingSenderId: "222117245630",
+  appId: "1:222117245630:web:f3a7a9c9542d8a14f2a4ef",
+  measurementId: "G-LHSJNNQP6C"
 };
 
 // Initialize Firebase
